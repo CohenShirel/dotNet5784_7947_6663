4512f1962b3b241b64e891bb0ea112f000c22542
shirel111 <shirel4201@gmail.com>
2023-12-28 22:36:40 +0200

    Initial Code+Welcome functions

---
+++ Stage0/Program6663.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        welcome7947();
+        welcome6663();
+        Console.ReadKey();
+    }
+
+    private static void welcome7947()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0},welcome to my first console application", name);
+    }
+    static partial void welcome6663();
+}
