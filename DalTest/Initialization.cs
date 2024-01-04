d98e7380e7db36b1e2ac24893737c1a9ee5ec0fa
noagol100 <noagol100@gmail.com>
2024-01-04 16:25:12 +0200

    h

---
+++ DalTest/Initialization.cs
@@ -0,0 +1,19 @@
+﻿using DalApi;
+using DO;
+namespace DalTest;
+
+public static class Initialization
+{
+    private static IWorker? s_dalWorker; //stage 1
+
+
+    //בנוסף, נצטרך שדה אחד, שכל הישויות יעשו בו שימוש, ליצירת מספרים רנדומליים בזמן מילוי ערכי האובייקטים. 
+
+    private static readonly Random s_rand = new();
+
+
+    private static void creatWorker ()
+    {
+
+    }
+}
