01853ce0eecd1b98fc941eda92a2ad92c3845c3e
shirel111 <shirel4201@gmail.com>
2024-01-09 21:29:35 +0200

    The final stage1!!!!!!!!!!!!!!!!!!!!!!!!!!

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -65,9 +65,9 @@ public static class Initialization
         string[] AssignmentssNames =
         {
            "confirmation","budget_planning","Architectural_design", "Building_foundations", "Sewage_infrastructure",
-           "Electrical_Systems", "floors","Climate_systems","Windows_doors","elevators","Emergency_systems",
-           "Automation_networks","Finishes_colors","Safety_systems","Technological_spaces","parkingLots","yards","External_installations",
-           "final_inspections","residents_Transferring"
+           "Electrical_Systems","walls","ceilings" ,"paving","Climate_systems","Windows_doors","elevators","Emergency_systems","plumbing",
+           "interior_design","Finishes_colors","Automation_networks","Safety_systems","External_installations","Technological_spaces",
+           "final_inspections","4Form","residents_Transferring"
         };
         foreach (var _name in AssignmentssNames)
         {
@@ -107,16 +107,46 @@ public static class Initialization
     //function that creat random links
     private static void creatLink()
     {
-        s_dalLink!.Create(new Link(0, 0, 1));
         s_dalLink!.Create(new Link(0, 1, 2));
         s_dalLink!.Create(new Link(0, 2, 3));
         s_dalLink!.Create(new Link(0, 3, 4));
-        s_dalLink!.Create(new Link(0, 3, 5));
-        s_dalLink!.Create(new Link(0, 5, 10));
-        s_dalLink!.Create(new Link(0, 10, 11));
-        s_dalLink!.Create(new Link(0, 11, 13));
-        s_dalLink!.Create(new Link(0, 13, 17));
-        s_dalLink!.Create(new Link(0, 17, 14));
-        s_dalLink!.Create(new Link(0, 18, 19));
+        s_dalLink!.Create(new Link(0, 4, 5));
+        s_dalLink!.Create(new Link(0, 4, 6));
+        s_dalLink!.Create(new Link(0, 5, 7));
+        s_dalLink!.Create(new Link(0, 5, 8));
+        s_dalLink!.Create(new Link(0, 6,7));
+        s_dalLink!.Create(new Link(0, 6,8));
+        s_dalLink!.Create(new Link(0, 7, 9));
+        s_dalLink!.Create(new Link(0, 7, 10));
+        s_dalLink!.Create(new Link(0, 7, 11));
+        s_dalLink!.Create(new Link(0, 7, 12));
+        s_dalLink!.Create(new Link(0, 7,13));
+        s_dalLink!.Create(new Link(0, 7, 14));
+        s_dalLink!.Create(new Link(0, 8,9));
+        s_dalLink!.Create(new Link(0, 8,10));
+        s_dalLink!.Create(new Link(0, 8,11));
+        s_dalLink!.Create(new Link(0, 8,12));
+        s_dalLink!.Create(new Link(0, 8,13));
+        s_dalLink!.Create(new Link(0, 8,14));
+        s_dalLink!.Create(new Link(0, 9,15));
+        s_dalLink!.Create(new Link(0, 9,17));
+        s_dalLink!.Create(new Link(0, 10,15));
+        s_dalLink!.Create(new Link(0, 10,17));
+        s_dalLink!.Create(new Link(0, 11,15));
+        s_dalLink!.Create(new Link(0, 11, 17));
+        s_dalLink!.Create(new Link(0, 12, 15));
+        s_dalLink!.Create(new Link(0, 12,17)); 
+        s_dalLink!.Create(new Link(0, 13,15));
+        s_dalLink!.Create(new Link(0, 13,17));
+        s_dalLink!.Create(new Link(0, 14,15));
+        s_dalLink!.Create(new Link(0, 14,17));
+        s_dalLink!.Create(new Link(0, 15, 16));
+        s_dalLink!.Create(new Link(0, 17,18));
+        s_dalLink!.Create(new Link(0, 18,19));
+        s_dalLink!.Create(new Link(0, 19,20));
+        s_dalLink!.Create(new Link(0, 20,21));
+        s_dalLink!.Create(new Link(0, 21, 22));
+        s_dalLink!.Create(new Link(0, 22, 23));
+        s_dalLink!.Create(new Link(0, 16, 21));
     }
 }
