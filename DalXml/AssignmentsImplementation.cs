8a863a723ab17a47e4c0afe5507b9c4304db330e
noagol100 <noagol100@gmail.com>
2024-01-19 00:21:19 +0200

    Continue step 5

--- DalXml/AssignmentsImplementation.cs
+++ DalXml/AssignmentsImplementation.cs
@@ -8,28 +8,70 @@ namespace Dal;
 internal class AssignmentsImplementation : IAssignments
 {
     readonly string s_assignmentss_xml = "assignmentss";
-    public int Create(Assignments item)
+    public int Create(Assignments ass)
     {
-        throw new NotImplementedException();
+
+    
+
+
+        List<DO.Assignments> assignmentss = XMLTools.LoadListFromXMLSerializer<DO.Assignments>(s_assignmentss_xml);
+
+        Assignments newAssignments = ass with { IdAssignments = Config.IdPAssignments };
+        assignmentss.Add(newAssignments);
+        XMLTools.SaveListToXMLSerializer<DO.Assignments>(assignmentss, s_assignmentss_xml);
+        return newAssignments.IdAssignments;
+
+        //if (Read(ass.IdAssignments) is not null)
+        //{
+        //    throw new DalAlreadyExistsException($"Assignments with id {ass.IdAssignments} does not exist");
+        //}
+
+
+        //return ass.IdAssignments;  
     }
 
     public void Delete(int id)
     {
-        
+
+      
+
+        List<DO.Assignments> assignmentss = XMLTools.LoadListFromXMLSerializer<DO.Assignments>(s_assignmentss_xml);
+        if (assignmentss.RemoveAll(item => item.IdAssignments == id) == 0)
+        {
+            throw new DalDoesNotExistException($"Assignments with id {id} does not exist");
+        }
+        XMLTools.SaveListToXMLSerializer<DO.Assignments>(assignmentss, s_assignmentss_xml);
     }
 
     public Assignments? Read(Func<Assignments, bool> filter)
     {
-        throw new NotImplementedException();
+        List<DO.Assignments> assignmentss = XMLTools.LoadListFromXMLSerializer<DO.Assignments>(s_assignmentss_xml);
+        //return assignmentss.FirstOrDefault(item => filter(item));
+        return assignmentss.FirstOrDefault(filter);
     }
 
     public IEnumerable<Assignments?> ReadAll(Func<Assignments, bool>? filter = null)
     {
-        throw new NotImplementedException();
+        List<DO.Assignments> assignmentss = XMLTools.LoadListFromXMLSerializer<DO.Assignments>(s_assignmentss_xml);
+
+        if (filter != null)
+        {
+            return from item in assignmentss
+                   where filter(item)
+                   select item;
+        }
+        return from item in assignmentss
+               select item;
     }
 
-    public void Update(ref Assignments item)
+    public void Update(ref Assignments ass)
     {
-        throw new NotImplementedException();
+        //List<DO.Assignments> assignmentss = XMLTools.LoadListFromXMLSerializer<DO.Assignments>(s_assignmentss_xml);
+
+
+        Delete(ass.IdAssignments);
+        Create(ass);
+        //XMLTools.SaveListToXMLSerializer<DO.Assignments>(assignmentss, s_assignmentss_xml);
+
     }
 }
