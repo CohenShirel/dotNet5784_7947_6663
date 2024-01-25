57d1aeaa223b955fca3b3526b4d15bd3bad93b69
shirel111 <shirel4201@gmail.com>
2024-01-25 22:30:28 +0200

    part7

---
+++ BL/BlApi/IAssignments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi;
+public interface IAssignments
+{
+    public int Create(BO.Assignments item);
+    public BO.Assignments? Read(int id);
+    public IEnumerable<BO.AssignmentsInList> ReadAll();
+    public void Update(BO.Assignments item);
+    public void Delete(int id);
+    public BO.WorkerInAssignments GetDetailedCourseForStudent(int WorkerId, int AssignmentsId);
+
+}
---
+++ BL/BlApi/IBI.cs
@@ -0,0 +1,11 @@
+﻿using BO;
+
+namespace BlApi;
+public interface IBl
+{
+    public IWorker Student { get; }
+    public IAssignments Course { get; }
+    public IWorkerInAssignments GradeSheet { get; }
+    public ISchedule Schedule { get; }
+}
+
