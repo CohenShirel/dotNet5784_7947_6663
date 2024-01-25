22899db2a6d7e267282ced94303325db1385844e
noagol100 <noagol100@gmail.com>
2024-01-25 22:31:25 +0200

    WE IN PART 7

---
+++ BL/BlApi/IWorker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi
+{
+    internal interface IWorker
+    {
+        public int Create(BO.Worker item);
+        public BO.Worker? Read(int id);
+        public IEnumerable<BO.WorkerInList> ReadAll();
+        public void Update(BO.Worker item);
+        public void Delete(int id);
+        public BO.WorkerInAssignments GetDetailedCourseForStudent(int StudentId, int CourseId);
+
+    }
+}
