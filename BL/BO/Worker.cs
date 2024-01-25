03cd82c8cc1ff0b2662cb848a86ddd874c899428
shirel111 <shirel4201@gmail.com>
2024-01-25 20:42:18 +0200

    to up

--- BL/BO/Worker.cs
+++ BL/BO/Worker.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace BO;
 public class Student
 {
+
     public int Id { get; init; }
     public string? Name { get; set; }
     public string? Email { get; set; }
