a23dc90b558f43a2365e86bfbdd32414493b29e9
shirel111 <shirel4201@gmail.com>
2024-01-25 20:15:06 +0200

    fix

---
+++ BL/BO/Assignments.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO;
+public class Assignments
+{
+
+}
+
+
+//int IdAssignments,
+//    int DurationAssignments,
+//  Level LevelAssignments,
+//    int IdWorker,
+//  DateOnly? dateSrart=null,
+//  DateOnly? DateBegin = null,
+//  DateOnly? DeadLine = null,
+//  DateOnly? DateFinish = null,
+//    string? Name = null,
+//    string? Description = null,
+//    string? Remarks = null,
+//    string? ResultProduct = null,
+//    bool Milestone = false
---
+++ BL/BO/Worker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO;
+public class Student
+{
+    public int Id { get; init; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public bool IsActive { get; set; }
+    DateTime? BirthDate { get; set; }
+    public DateTime RegistrationDate { get; init; }
+    Year CurrentYear { get; init; } // (Year)(DateTime.Now.Year - RegistrationDate.Year);
+    //אתגררררררררררררר
+    //public override string ToString() => this.ToStringProperty();
+}
+//int IdWorker,///////////////////////////////
+//    Level Experience,
+//    int HourSalary,
+//    string? Name = null,////////////////////
+//    string? Email = null//////////////////
