557cb1c62ce52fc921e77b298f3dd431c69eea52
shirel111 <shirel4201@gmail.com>
2024-02-22 00:09:10 +0200

    syncren

--- PL/Worker/WorkerListWindow.xaml.cs
+++ PL/Worker/WorkerListWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using BO;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,30 +18,46 @@ namespace PL.Worker
 {
     public partial class WorkerListWindow : Window
     {
-        public WorkerListWindow()
-        {
-            InitializeComponent();
-            WorkerList = s_bl?.Worker.ReadAll()!;
-            //ComboBox.ItemsSource=Enums.GetValues(typeof(DO.Level));
-        }
         public DO.Level level { get; set; } = DO.Level.None;
 
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
-        public IEnumerable<BO.WorkerInList> WorkerList
+
+        //ObservableCollection <BO.WorkerInList> lsttWorker;
+        public ObservableCollection<IEnumerable<BO.WorkerInList>> ObserveListWorker
         {
-            get { return (IEnumerable<BO.WorkerInList>)GetValue(WorkerListProperty); }
-            set { SetValue(WorkerListProperty, value); }
+            get { return (ObservableCollection<IEnumerable<BO.WorkerInList>>)GetValue(observeListWorkerProperty); }
+            set { SetValue(observeListWorkerProperty, value); }
+        }
+        //יודעת לדווח על הגרפיקה על קיומה...מדווחת על שינויים על כל הוספה או מחיקה של מישהו
+        public static readonly DependencyProperty observeListWorkerProperty =
+            DependencyProperty.Register("ObserveListWorker", typeof(ObservableCollection<IEnumerable<BO.WorkerInList>>), typeof(WorkerListWindow), new PropertyMetadata(null));
+        //public IEnumerable<BO.WorkerInList> WorkerList
+        //{
+        //    get { return (IEnumerable<BO.WorkerInList>)GetValue(WorkerListProperty); }
+        //    set { SetValue(WorkerListProperty, value); }
+        //}
+
+        //public static readonly DependencyProperty WorkerListProperty =
+        //    DependencyProperty.Register("WorkerList", typeof(IEnumerable<BO.WorkerInList>), typeof(WorkerListWindow), new PropertyMetadata(null));
+        public WorkerListWindow()
+        {
+            InitializeComponent();
+            ObserveListWorker = s_bl?.Worker.ReadAll()!;
+            //ComboBox.ItemsSource=Enums.GetValues(typeof(DO.Level));
         }
 
-        public static readonly DependencyProperty WorkerListProperty =
-            DependencyProperty.Register("WorkerList", typeof(IEnumerable<BO.WorkerInList>), typeof(WorkerListWindow), new PropertyMetadata(null));
        
 
         private void Cmb_Levels(object sender, SelectionChangedEventArgs e)
         {
-            WorkerList = (level == DO.Level.None) ?
+            ObserveListWorker = (level == DO.Level.None) ?
             s_bl?.Worker.ReadAll()! : s_bl?.Worker.ReadAll(item => item.Experience == level)!;
 
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            new WorkerWindow().Show();
         }
     }
 }
